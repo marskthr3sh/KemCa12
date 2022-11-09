e6f94a7f18f8910336cae83401ce10fca649d799
marskthr3sh <117838665+marskthr3sh@users.noreply.github.com>
2022-11-09 22:07:30 +0700

    Initial commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Hello Kem Cá",
+  descriptionHeader:
+    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đó :))) ",
+  buttonYes: "Đồng Ý Đê",
+  buttonNo: "Kó ",
+  titleModar: "Cảm ơn cậu đã đồng ý ha",
+  descriptionModar:
+    'Biết ngay là cậu sẽ đồng ý làm người yêu Mình mà ^^ </br> tôi muốn em là của riêng mình :)) và cậu sẽ được trân quý </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  đồ hay giận "',
+};
